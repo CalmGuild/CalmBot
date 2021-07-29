e0df576f226c4263784b1a168f2f3e677facb7da
Miqhtiedev <miqhtiedev@gmail.com>
2021-07-29 18:42:25 -0400

    tickets data in guild settings

--- src/schemas/GuildSettings.ts
+++ src/schemas/GuildSettings.ts
@@ -1,15 +1,22 @@
+import { Collection } from "discord.js";
 import { Schema, model, Document } from "mongoose";
 
 export interface IGuildSettings extends Document {
   guildID: string;
   disabledCommands: Array<string>;
   sleep: Boolean;
+  tickets: Collection<string, string>;
+  ticketRoles: Array<string>;
+  ticketCount: number;
 }
 
 const GuildSettingsScema = new Schema({
   guildID: { type: String, requried: true, index: true, unique: true },
   disabledCommands: { type: Array<string>(), default: new Array<String>() },
   sleep: { type: Boolean, default: false },
+  tickets: { type: Map, default: new Map() },
+  ticketRoles: { type: Array<string>(), default: new Array<String>() },
+  ticketCount: { type: Number, default: 0 },
 });
 
 export default model<IGuildSettings>("GuildSettings", GuildSettingsScema);
