6ebacbdee8ae4444bb6457a028cd91e319d788bf
Szarak2004 <czarq110@gmail.com>
2021-07-30 13:25:46 +0200

    Changed Ping! to Pong!

--- src/commands/Utility/ping.ts
+++ src/commands/Utility/ping.ts
@@ -2,7 +2,7 @@ import { ICommandSettings } from "../../structures/interfaces";
 
 const command: ICommandSettings = {
   run: (client, message, args) => {
-    client.reply(message, `Ping! Latency: ${Date.now() - message.createdTimestamp}ms.`);
+    client.reply(message, `Pong! Latency: ${Date.now() - message.createdTimestamp}ms.`);
   },
   description: "Pong!",
   usage: "ping",
