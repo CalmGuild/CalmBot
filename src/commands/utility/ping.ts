4f4d7dc45012a1a08b56dd2b3c75109795f7e2d5
Miqhtiedev <miqhtiedev@gmail.com>
2021-08-19 12:04:24 -0400

    fix(ping): latency showing negative numbers

--- src/commands/utility/ping.ts
+++ src/commands/utility/ping.ts
@@ -2,7 +2,10 @@ import { ICommandSettings } from "../../structures/interfaces";
 
 const command: ICommandSettings = {
   run: (client, message, args) => {
-    client.reply(message, `Pong! Latency: ${Date.now() - message.createdTimestamp}ms.`);
+    const ts = Date.now();
+    message.reply("Pinging...").then((msg) => {
+      msg.edit(`Pong! Latency: ${Date.now() - ts}`)
+    });
   },
   description: "Pong!",
   usage: "ping",
