6dc43a92c559fdd066d95edb128610ee4e82144b
Miqhtiedev <miqhtiedev@gmail.com>
2021-09-07 11:13:15 -0400

    remove comment from unlink.ts

--- src/commands/admin/unlink.ts
+++ src/commands/admin/unlink.ts
@@ -1,11 +1,3 @@
-/*
-    let member: GuildMember | void;
-    if (new RegExp(MessageMentions.USERS_PATTERN).test(args[0]!!)) {
-      const id = args[0]!!.substring(3, args[0]!!.length - 1);
-      member = await message.guild.members.fetch(id).catch(() => {});
-    } else member = await message.guild.members.fetch(args[0]!!).catch(() => {});
-*/
-
 import { MessageMentions, User as DiscordUser } from "discord.js";
 import User, { IUser } from "../../schemas/User";
 import { ICommandSettings } from "../../structures/interfaces";
