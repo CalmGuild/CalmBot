3f814451451aa02a28c6cbc8e47b713aa82f8b2c
DVG <55554025+IAmDVG@users.noreply.github.com>
2022-02-01 15:46:38 -0500

    Update ping.ts

--- src/commands/utility/ping.ts
+++ src/commands/utility/ping.ts
@@ -3,7 +3,7 @@ import { ICommandSettings } from "../../structures/interfaces";
 const command: ICommandSettings = {
   run: (client, message, args) => {
     const ts = Date.now();
-    message.channel.send("Pinging...").then((msg) => {
+    message.reply("Pinging...").then((msg) => {
       msg.edit(`Pong! Latency is ${Date.now() - ts}ms. API Latency is ${client.ws.ping}ms`)
     });
   },
