97b68f5d6dfab31cd74507d6afc56a2a27da120c
Super <59453528+oSuper@users.noreply.github.com>
2022-08-25 10:47:52 +0000

    Update poop.ts

---
+++ src/commands/fun/poop.ts
@@ -0,0 +1,14 @@
+import { ICommandSettings } from "../../structures/interfaces";
+import constants from "../../util/constants";
+import Utils from "../../util/Utils";
+
+const command: ICommandSettings = {
+  run: (client, message, args) => {
+  client.reply("💩")
+  },
+  description: "Get a poop reply",
+  usage: "poop",
+  aliases: ["poo"],
+  };
+  
+  export default commands
