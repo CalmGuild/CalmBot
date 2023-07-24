0ba1746aa949dbb726c3d870a0302e3e9e70f7d5
Clayton <miqhtiedev@gmail.com>
2023-07-24 01:17:15 -0400

    allow you to change a suggestion from denied to accepted and vise-versa

--- src/util/suggestion.ts
+++ src/util/suggestion.ts
@@ -1,18 +1,18 @@
-import { Message, Client, MessageEmbed, GuildMember } from "discord.js";
+import { Message, Client, MessageEmbed, GuildMember, EmbedFieldData, EmbedField } from "discord.js";
 
 export const editSuggestion = async (message: Message, editor: GuildMember, action: "ACCEPT" | "DENY"): Promise<boolean> => {
   return new Promise(async (resolve, reject) => {
+    if (!message.embeds[0]) return;
     const accept = action === "ACCEPT";
 
-    const upvotes = await message.reactions.resolve("✅")?.fetch();
-    const dwonvotes = await message.reactions.resolve("❎")?.fetch();
-
     const newEmbed = new MessageEmbed(message.embeds[0]).setColor(accept ? "GREEN" : "RED").setTitle(`Suggestion: ${accept ? "Accepted" : "Denied"}`);
-    newEmbed.addFields([
-      { name: `${accept ? "Accepted" : "Denied"} by:`, value: editor.user.toString(), inline: true },
-      { name: "Upvotes:", value: upvotes?.count.toString() ?? "0", inline: true },
-      { name: "Downvotes:", value: dwonvotes?.count.toString() ?? "0", inline: true },
-    ]);
+
+    const fields: EmbedFieldData[] = [{ name: `${accept ? "Accepted" : "Denied"} by:`, value: editor.user.toString(), inline: true }];
+    
+    const votes = message.embeds[0].fields.filter((field) => field.name == "Upvotes:" || field.name == "Downvotes:");
+    if (votes.length == 0) votes.push(...(await getVotes(message)));
+
+    newEmbed.setFields(fields, votes);
 
     message
       .edit({ embeds: [newEmbed] })
@@ -25,3 +25,14 @@ export const editSuggestion = async (message: Message, editor: GuildMember, acti
 };
 
 export const validateSuggestion = (client: Client, message: Message): boolean => message.embeds.length === 1 && (message.embeds[0]?.title?.startsWith("Suggestion:") ?? false) && message.author.id === client.user?.id;
+
+const getVotes = async (message: Message): Promise<EmbedField[]> =>
+  new Promise(async (resolve, reject) => {
+    const upvotes = await message.reactions.resolve("✅")?.fetch();
+    const downvotes = await message.reactions.resolve("❎")?.fetch();
+
+    resolve([
+      { name: "Upvotes:", value: upvotes?.count.toString() ?? "0", inline: true },
+      { name: "Downvotes:", value: downvotes?.count.toString() ?? "0", inline: true },
+    ]);
+  });
